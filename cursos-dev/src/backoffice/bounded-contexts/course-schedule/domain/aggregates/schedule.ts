55f4b888672b4f816362d80cb59a8f113648218d
Sergio Hidalgo <shidalgoc@auna.pe>
2022-09-11 03:17:10 -0500

    aggregate schedule

---
+++ cursos-dev/src/backoffice/bounded-contexts/course-schedule/domain/aggregates/schedule.ts
@@ -0,0 +1,82 @@
+import { AggregateRoot } from '@nestjs/cqrs';
+export type ScheduleEssential = {
+  readonly id: string;
+  readonly courseId: string;
+  readonly subject: string;
+  readonly status: string;
+};
+
+export type ScheduleOptional = {
+  readonly frequency: string;
+  readonly duration: string;
+  readonly startDate: Date;
+  readonly phrase: string;
+  readonly timeStartAndEnd: string;
+  readonly zoomId: string;
+};
+
+export type ScheduleUpdate = {
+  readonly subject: string;
+  readonly status: string;
+  readonly frequency: string;
+  readonly duration: string;
+  readonly startDate: Date;
+  readonly phrase: string;
+  readonly timeStartAndEnd: string;
+  readonly zoomId: string;
+};
+
+export type ScheduleProperties = Required<ScheduleEssential> &
+  Partial<ScheduleOptional>;
+
+export class Schedule extends AggregateRoot {
+  private readonly id: string;
+  private readonly courseId: string;
+  private subject: string;
+  private status: string;
+  private frequency: string;
+  private duration: string;
+  private startDate: Date;
+  private phrase: string;
+  private timeStartAndEnd: string;
+  private zoomId: string;
+  private active: boolean;
+  private readonly createdAt: Date;
+  private updatedAt: Date;
+  private deletedAt: Date;
+
+  constructor(properties: ScheduleProperties) {
+    super();
+    Object.assign(this, properties);
+    this.createdAt = new Date();
+  }
+
+  properties() {
+    return {
+      id: this.id,
+      courseId: this.courseId,
+      subject: this.subject,
+      status: this.status,
+      frequency: this.frequency,
+      duration: this.duration,
+      startDate: this.startDate,
+      phrase: this.phrase,
+      timeStartAndEnd: this.timeStartAndEnd,
+      zoomId: this.zoomId,
+      active: this.active,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt,
+      deletedAt: this.deletedAt,
+    };
+  }
+
+  update(fields: Partial<ScheduleUpdate>) {
+    Object.assign(this, fields);
+    this.updatedAt = new Date();
+  }
+
+  delete() {
+    this.active = false;
+    this.deletedAt = new Date();
+  }
+}
